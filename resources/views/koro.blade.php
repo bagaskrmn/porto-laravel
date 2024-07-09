6db1a7580d8ba0b8ae53a2f2adb709cb36f5cae9
bagaskrmn <bagaskaramnoer@gmail.com>
2024-07-10 02:19:40 +0700

    add views2

---
+++ resources/views/koro.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KORO</title>
+</head>
+<body>
+    <h1>KORO YANG TERLUPAKAN</h1>
+    <a href="/index" class="btn">Balik Ke FIra</a>
+</body>
+</html>
